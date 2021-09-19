0e285a2a93fcd5287da1dcfc6b830169982df896
Kamimura Akito <akito0702kamimura@gmail.com>
2021-09-19 03:48:25 +0000

    テンプレートのベース完成

--- blog_kamimura/views.py
+++ blog_kamimura/views.py
@@ -2,4 +2,4 @@ from django.views import generic
 
 # Create your views here.
 class IndexView(generic.TemplateView):
-    template_name = "blog_kamimura/base.html"
+    template_name = "blog_kamimura/index.html"
