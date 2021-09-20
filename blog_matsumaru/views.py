fef38aae78da09c1f65b5355e0c43ea54f27807e
Naoki <freeadores@gmail.com>
2021-09-21 00:27:25 +0900

    お問合せページにメール送信機能を実装

--- blog_matsumaru/views.py
+++ blog_matsumaru/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView
+from django.urls import reverse_lazy
+from django.contrib import messages 
 from .forms import InquiryForm
 
 
@@ -9,3 +11,9 @@ class IndexView(TemplateView):
 class InquiryView(FormView):
   template_name = 'blog_matsumaru/inquiry.html'
   form_class    = InquiryForm
+  success_url   = reverse_lazy('blog_matsumaru:inquiry')
+
+  def form_valid(self, form):
+    form.send_email()
+    messages.success(self.request, 'メッセージを送信しました。')
+    return super().form_valid(form)
