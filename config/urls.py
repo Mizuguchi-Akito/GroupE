8b32218b313cc76b08cb96b5e0121b3df237bed9
Naoki <freeadores@gmail.com>
2021-09-21 17:22:39 +0900

    settings.pyの調整

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from . import settings
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
